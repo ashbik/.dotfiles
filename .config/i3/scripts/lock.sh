b91f46664237f22bdf2fce79ccd9beb7901e81d2
ashbik <ashbik@live.com>
2020-03-05 14:42:32 +0545

    Auto updated using vim (see .vimrc for details.)

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-import -window root $HOME/.cache/screenshot.png 
-convert -scale 10% -scale 1000% $HOME/.cache/screenshot.png $HOME/.cache/screenshot.png 
-i3lock -i $HOME/.cache/screenshot.png &
+import -window root $HOME/.cache/sc.png 
+convert $HOME/.cache/sc.png -channel RGB -blur 0x4 -gravity center $HOME/Cloud/Images/Wall/lock.png -composite $HOME/.cache/sc.png   
+i3lock -i $HOME/.cache/sc.png &
 exit 0 
