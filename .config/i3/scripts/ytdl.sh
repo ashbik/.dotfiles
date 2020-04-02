e54a1e663a492cc35efd324f96a37d42b5111434
ashbik <ashbik@live.com>
2020-04-02 10:02:50 +0545

    Auto updated using vim (see .vimrc for details.)

--- .config/i3/scripts/ytdl.sh
+++ .config/i3/scripts/ytdl.sh
@@ -6,35 +6,36 @@ URL=$(eval $x)
 
 DES="$HOME/Videos/YTdl/%(title)s.%(ext)s"
 
-chosenqu=$(echo -e "Best_Quality[video/mp4]\nMid_Quality[video/mp4]\nLowQuality[video/mp4]\naudio" | dmenu -i -p "Select desired format.")
-
-y="youtube-dl --get-filename -o '%(title)s' $URL"
-eval $y
-FN=$(eval $y)
+SE=$(echo -e "Best_Quality[video/mp4]\nMid_Quality[video/mp4]\nLowQuality[video/mp4]\naudio\nStream(1080p)" | dmenu -i -p "Select desired format.")
+FN="find $HOME/Videos/YTdl/ -type f -cmin -0.10 | grep "YTdl/" | cut -d"/" -f6"
 
 
-if [ "$chosenqu" == "Best_Quality[video/mp4]" ]; then
+if [ "$SE" == "Best_Quality[video/mp4]" ]; then
 
-	youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio --merge-output-format mp4 "$URL" -o "$DES" && notify-send " Download Completed !!! " " ( $FN ) has been downloaded successfully."
+        youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio --merge-output-format mp4 "$URL" -o "$DES" && 
+	notify-send " Download Complete. " "`eval $FN`"
 
 
-elif [ "$chosenqu" == "Mid_Quality[video/mp4]" ]; then
-	
-	youtube-dl -f 22 "$URL" -o "$DES" && 
-	notify-send " Download Completed !!! " " ( $FN ) has been downloaded successfully."
+elif [ "$SE" == "Mid_Quality[video/mp4]" ]; then
 
-elif [ "$chosenqu" == "Low_Quality[video/mp4]" ]; then
+        youtube-dl -f 22 "$URL" -o "$DES" &&
+        notify-send " Download Complete. " "`eval $FN`"
 
-	youtube-dl -f 135+140 "$URL" -o "$DES" && 
-	notify-send " Download Completed !!! " " ( $FN ) has been downloaded successfully."
+elif [ "$SE" == "Low_Quality[video/mp4]" ]; then
 
-elif [ "$chosenqu" == "audio" ]; then
+        youtube-dl -f 135+140 "$URL" -o "$DES" &&
+        notify-send " Download Complete. " "`eval $FN`"
 
-	youtube-dl -f 140 "$URL" -o "$DES" &&
-	 notify-send " Download Completed !!! " " ( $FN ) has been downloaded successfully."
+elif [ "$SE" == "audio" ]; then
+
+        youtube-dl -f 140 "$URL" -o "$DES" &&
+         notify-send " Download Complete. " "`eval $FN`"
+
+elif [ "$SE" == "Stream" ]; then
+	notify-send " Streaming $URL locally."
+        mpv   --ytdl-format=bestvideo[height=1080]+bestaudio/best --geometry=70%x70%  "$URL" &&
+        notify-send "  Streaming Closed."
 else
-	 notify-send "Process Aborted !!! " "Could not download video. ( $FN )"
+         notify-send " Process Stopped. " "Could not download the url $URL."
 fi
-
-
 
